e7346051ef2da88ba963dae947474747afae4501
popking159 <47087407+popking159@users.noreply.github.com>
2026-09-21 23:36:26 +0300

    Update version number to 2.7 in myinstaller.sh

--- myinstaller.sh
+++ myinstaller.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ######### Only These two lines to edit with new version #####
-version=2.5
+version=2.7
 #############################################################
 PLUGINPATH='/usr/lib/enigma2/python/Plugins/Extensions/ArabicSavior'
 
